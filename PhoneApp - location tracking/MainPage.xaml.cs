58ec1a056e2bbb860a2fbf8ebfb380cf3fa95c78
wangyuanhsiang <ywang91@studentmail.nait.ca>
2016-02-24 19:49:23 -0700

    Update sample - shawn 02/24/2016

---
+++ PhoneApp - location tracking/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp___location_tracking.Resources;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using System.IO.IsolatedStorage;
+
+namespace PhoneApp___location_tracking
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Geolocator geolocator = null;
+        bool tracking = false;
+        public MainPage()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if(IsolatedStorageSettings.ApplicationSettings.Contains("LocationConsent"))
+            {
+                return;
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("THis app accesses your phone's location. Is it that ok?","Location",MessageBoxButton.OKCancel);
+                if(result == MessageBoxResult.OK)
+                {
+                    IsolatedStorageSettings.ApplicationSettings["LocationConsent"] = true;
+                }
+                else
+                {
+                    IsolatedStorageSettings.ApplicationSettings["LocationConsent"] = false;
+                }
+                IsolatedStorageSettings.ApplicationSettings.Save();
+            }
+
+        }
+
+        private void TrackLocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if((bool)IsolatedStorageSettings.ApplicationSettings["LocationConsent"] != true)
+            {
+                return;
+            }
+            if(!tracking)
+            {
+                geolocator = new Geolocator();
+                geolocator.DesiredAccuracy = PositionAccuracy.High;
+                geolocator.MovementThreshold = 100;
+
+                geolocator.StatusChanged += geolocator_StatusChanged;
+                geolocator.PositionChanged += geolocator_PositionChanged;
+
+                tracking = true;
+                TrackLocationButton.Content = "stop tracking";
+            }
+            else
+            {
+                geolocator.PositionChanged -= geolocator_PositionChanged;
+                geolocator.StatusChanged -= geolocator_StatusChanged;
+                geolocator = null;
+                tracking = false;
+                TrackLocationButton.Content = "track location";
+                StatusTextBlock.Text = "stopped";
+            }
+        }
+
+        private void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                LatitudeTextBlock.Text = args.Position.Coordinate.Latitude.ToString("0.00");
+                LongitudeTextBlock.Text = args.Position.Coordinate.Longitude.ToString("0.00");
+            });
+        }
+
+        void geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+            string status = "";
+            switch (args.Status)
+            {
+                case PositionStatus.Disabled:
+                    // the application does not have the right capability or the location master switch is off
+                    status = "location is disabled in phone settings";
+                    break;
+                case PositionStatus.Initializing:
+                    // the geolocator started the tracking operation
+                    status = "initializing";
+                    break;
+                case PositionStatus.NoData:
+                    // the location service was not able to acquire the location
+                    status = "no data";
+                    break;
+                case PositionStatus.Ready:
+                    // the location service is generating geopositions as specified by the tracking parameters
+                    status = "ready";
+                    break;
+                case PositionStatus.NotAvailable:
+                    status = "not available";
+                    // not used in WindowsPhone, Windows desktop uses this value to signal that there is no hardware capable to acquire location information
+                    break;
+                case PositionStatus.NotInitialized:
+                    // the initial state of the geolocator, once the tracking operation is stopped by the user the geolocator moves back to this state
+
+                    break;
+            }
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                StatusTextBlock.Text = status;
+            });
+        }
+
+
+
+       
+    }
+}
